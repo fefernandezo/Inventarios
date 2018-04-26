820d8d611b1f9e92afdb942f5be12f9d4b246789
Francisco E. Fernández Ortiz <franciscofernandezortiz@hotmail.com>
2018-04-26 12:53:13 -0300

    subida de archivos

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+        ZXingScannerPage scanPage;
+		public MainPage()
+		{
+			InitializeComponent();
+            btnScann.Clicked += BtnScann_Clicked;
+		}
+
+        private async void BtnScann_Clicked(object sender, EventArgs e)
+        {
+            scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += (result) => {
+                scanPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopModalAsync();
+                    DisplayAlert("Codigo", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushModalAsync(scanPage);
+        }
+    }
+}
